0da75d5f334236d6cc207a896760caa4d2d66500
devfelipevitorino <devfelipevitorino@gmail.com>
2025-06-28 00:40:38 -0300

    feat(database): conexao com o banco

---
+++ database/database.php
@@ -0,0 +1,16 @@
+<?php
+
+$serverName = 'localhost';
+$usuario = 'root';
+$senha = '';
+$db_nome = 'db_corte_facil';
+
+$conn = new mysqli($serverName, $usuario, $senha, $db_nome);
+
+if ($conn->connect_error) {
+    die('Falha na conexão com o banco: ' . $conn->connect_error);
+}
+
+echo "Conectado com sucesso";
+
+$conn->close();
