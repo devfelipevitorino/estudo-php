5776f9695b7e8520f92685698ca28e6a66e09b03
devfelipevitorino <devfelipevitorino@gmail.com>
2025-06-29 13:55:08 -0300

    fix(css/html): Estilizar tela de login

--- pages/index.php
+++ pages/index.php
@@ -3,29 +3,33 @@
 
 <head>
     <?php include 'componentes/head.php'; ?>
-    <link rel="stylesheet" href="../css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Sistema</title>
+    <link rel="stylesheet" href="../css/login.css">
 </head>
 
 <body>
     <?php include '../database/database.php' ?>
-    <div>
-        <div>
-            <h2>Acesso ao Sistema</h2>
+
+    <div class="login-container">
+        <div class="form-box">
+            <h2 class="titulo-h2">Acesso ao Sistema</h2>
             <form action="../database/login_process.php" method="POST">
-                <div>
-                    <input type="text" id="usuario" name="usuario" placeholder="Usuário" required>
+                <div class="form-input">
+                    <input type="text" id="usuario" name="usuario" required>
                     <label for="usuario">Usuário</label>
                 </div>
-                <div>
-                    <input type="password" id="senha" name="senha" placeholder="Senha" required>
+                <div class="form-input">
+                    <input type="password" id="senha" name="senha" required>
                     <label for="senha">Senha</label>
                 </div>
-                <button type="submit" name="btn_login">Entrar</button>
+                <button class="btn_login" type="submit" name="btn_login">Entrar</button>
             </form>
         </div>
+        <footer class="footer">
+            <?php include 'componentes/footer.php'; ?>
+        </footer>
     </div>
-
-    <?php include 'componentes/footer.php'; ?>
-
 </body>
 </html>
